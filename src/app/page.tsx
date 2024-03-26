f2a1706bae10c6350cee890d965ccdd0756765d7
jojo <perebaj@gmail.com>
2024-03-26 00:11:19 -0300

    :bug: chore: fix font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <header>
-          <nav className="mx-auto flex max-w-7xl items-center justify-center px-32 py-3 text-[12px]">
+          <nav className="mx-auto flex max-w-7xl items-center justify-center px-32 py-3 text-xl">
             <span className="flex-1">SyncSpot</span>
             <div className="flex items-center justify-center gap-4">
               <a href="/#faq" className="no-underline hover:underline">
@@ -22,7 +22,7 @@ export default function Home() {
                 whileHover={{ scale: 1.2 }}
                 whileTap={{ scale: 0.9 }}
               >
-                <Button size={'sm'} className="text-white">
+                <Button size={'default'} className="text-white">
                   Login
                 </Button>
               </motion.button>
@@ -34,20 +34,20 @@ export default function Home() {
           animate={{ opacity: 1, scale: 1.2 }}
           transition={{ ease: 'easeOut', duration: 1 }}
         >
-          <section className="relative mx-auto flex max-w-2xl flex-col items-center gap-2 p-8 pt-16">
-            <h2 className="flex flex-col text-center text-3xl font-medium text-slate-600">
+          <section className="relative mx-auto flex max-w-5xl flex-col items-center p-8 pt-32">
+            <h2 className="flex flex-col text-center text-5xl font-medium text-slate-600 ">
               The easiest way,{' '}
-              <span className="bg-gradient-to-r from-yellow-300 from-20% via-yellow-400 to-yellow-600 to-70% bg-clip-text text-transparent">
+              <span className="bg-gradient-to-r from-yellow-300 from-20% via-yellow-400 to-yellow-600 to-70% bg-clip-text pb-6 text-transparent">
                 to manage your meetings time
               </span>
             </h2>
             <span className="text-xl font-light">
-              Quickly setup bulletpoints to focus on during your meetings
+              Setup bulletpoints to focus on during your meetings
             </span>
           </section>
         </motion.div>
         <footer>
-          <div className="flex flex-col items-center justify-center py-4 text-[8px]">
+          <div className="flex flex-col items-center justify-center py-4 text-sm">
             <h4 className="text-slate-300">
               Made with ❤️ by{' '}
               <a
